8e6c6d11a1089d10a574674cb45c10cc6812024e
XantX <hp_xantx@hotmail.com>
2019-11-09 23:42:13 -0500

    cambios en informe

--- Proyecto_final_algoritmos/Proyecto final algoritmos/Source.cpp
+++ Proyecto_final_algoritmos/Proyecto final algoritmos/Source.cpp
@@ -1,27 +1,30 @@
+#include <io.h>
 #include <iostream>
 #include <string>
 #include "dirent.h"
+using namespace System::IO;
 using namespace std;
 void Litar(string Dir) {//ruta
 	DIR*directorio;
 	struct dirent*elemto;
 	string nombre;
-	long numero;
+	long size;
 	if (directorio = opendir(Dir.c_str())) {
 		while (elemto = readdir(directorio)) {
 			nombre = elemto->d_name;
-			numero = elemto->d_type;
-			if (nombre != "."&& nombre != "..") { cout << nombre <<" "<< numero << endl; }
+			
+			if (nombre != "."&& nombre != "..") { cout << nombre <<" "<< size << endl; }
 		}
 	}
 	closedir(directorio);
 }
 void init() {
-	cout << "ruta: " << endl;
+	cout << "dime las ruta para escanear: " << endl;
 		string dir;
 		getline(cin, dir);
 		Litar(dir);
 }
+
 int main() {
 	init();
 	cin.get();
