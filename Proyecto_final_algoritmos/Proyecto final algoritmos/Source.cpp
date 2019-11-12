1d8b53bb1f3e96d5948d9a8e93ebe85a2d5f2e63
XantX <hp_xantx@hotmail.com>
2019-11-11 20:48:05 -0500

    pruebas de escaneo

--- Proyecto_final_algoritmos/Proyecto final algoritmos/Source.cpp
+++ Proyecto_final_algoritmos/Proyecto final algoritmos/Source.cpp
@@ -2,31 +2,37 @@
 #include <iostream>
 #include <string>
 #include "dirent.h"
+#include <vector>
+
+using namespace System;
 using namespace System::IO;
-using namespace std;
-void Litar(string Dir) {//ruta
-	DIR*directorio;
-	struct dirent*elemto;
-	string nombre;
-	long size;
-	if (directorio = opendir(Dir.c_str())) {
-		while (elemto = readdir(directorio)) {
-			nombre = elemto->d_name;
-			
-			if (nombre != "."&& nombre != "..") { cout << nombre <<" "<< size << endl; }
-		}
+
+void Litar(String^ Dir){//ruta
+
+    // Make a reference to a directory.
+	DirectoryInfo^ di = gcnew DirectoryInfo(Dir);
+
+	// Get a reference to each file in that directory.
+	array<FileInfo^>^fiArr = di->GetFiles();
+
+	// Display the names and sizes of the files.
+	Console::WriteLine("The directory {0} contains the following files:", di->Name);
+	System::Collections::IEnumerator^ myEnum = fiArr->GetEnumerator();
+	while (myEnum->MoveNext())
+	{
+		FileInfo^ f = safe_cast<FileInfo^>(myEnum->Current);
+		Console::WriteLine("The size of {0} is {1} bytes.", f->Name, f->Length);
 	}
-	closedir(directorio);
-}
-void init() {
-	cout << "dime las ruta para escanear: " << endl;
-		string dir;
-		getline(cin, dir);
-		Litar(dir);
 }
 
+
 int main() {
-	init();
-	cin.get();
+	std::string Dir;
+	std::cout << "dime la direccion" << std::endl;
+	getline(std::cin, Dir);
+	String^dir=gcnew String(Dir.c_str());
+	Litar(dir);
+
+	std::cin.get();
 	return 0;
 }
